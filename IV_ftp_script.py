30bfc1b24bcc49e0d0b54a333e26ca2ce170f4a0
owinton <43796798+owinton@users.noreply.github.com>
2022-02-02 10:12:59 +0100

    Changes IV_ftp to test on DLPTest, a free public ftp server for testing

--- IV_ftp_script.py
+++ IV_ftp_script.py
@@ -7,13 +7,16 @@ Created by kaha on 25 Jun 2018
 Last edited by oew on 10 Mar 2021
 """
 def IV_ftp_upload(output_plots_to_upload, output_plots_uploaded):
-	ftp = ftplib.FTP('ftpserver.dmi.dk')
+	#ftp = ftplib.FTP('ftpserver.dmi.dk')
+	ftp = ftplib.FTP('ftp.dlptest.com')
     with open('../username_password.txt', 'r') as file:
         data = file.readlines()
         data = [d.replace('\n', '') for d in data]
     USERNAME = data[2]
     PASSWORD = data[3]
-	ftp.login(USERNAME, PASSWORD)	ftp.cwd('upload')
+	#ftp.login(USERNAME, PASSWORD)	
+	ftp.login('dlpuser', 'rNrKYTX9g7z3RgJRmxWuGHbeu')
+	#ftp.cwd('upload')
 	upload_list = [i for i in output_plots_to_upload if i.endswith('.png')]
 	for upload_filename in upload_list:
 		file = open(os.path.join(output_plots_to_upload, upload_filename, 'rb')
